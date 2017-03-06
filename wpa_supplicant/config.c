c8ae5dba7261f5ff58065acf3e3f9453818c3582
Kanchanapally, Vidyullatha <vkanchan@qti.qualcomm.com>
2017-03-06 17:16:00 +0530

    MBO: Add support for transition reject reason code
    Add support for rejecting a BSS transition request using MBO reject
    reason codes. A candidate is selected or rejected based on whether it is
    found acceptable by both wpa_supplicant and the driver. Also accept any
    candidate meeting a certain threshold if disassoc imminent is set in BTM
    Request frame.
    
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    Git-commit: af8bc24da37c340e17417317d7d97f1ea2ef0dc3
    Git-repo : git://w1.fi/srv/git/hostap.git
    Change-Id: Ibed6b88b777f8254a641c9df034864a857cd7f2d
    CRs-Fixed: 2015867

--- wpa_supplicant/config.c
+++ wpa_supplicant/config.c
@@ -3658,6 +3658,8 @@ struct wpa_config * wpa_config_alloc_empty(const char *ctrl_interface,
 
 #ifdef CONFIG_MBO
 	config->mbo_cell_capa = DEFAULT_MBO_CELL_CAPA;
+	config->disassoc_imminent_rssi_threshold =
+		DEFAULT_DISASSOC_IMMINENT_RSSI_THRESHOLD;
 #endif /* CONFIG_MBO */
 
 	if (ctrl_interface)
@@ -4378,6 +4380,7 @@ static const struct global_parse_data global_fields[] = {
 	{ STR(non_pref_chan), 0 },
 	{ INT_RANGE(mbo_cell_capa, MBO_CELL_CAPA_AVAILABLE,
 		    MBO_CELL_CAPA_NOT_SUPPORTED), 0 },
+	{ INT_RANGE(disassoc_imminent_rssi_threshold, -120, 0), 0 },
 #endif /*CONFIG_MBO */
 	{ INT(gas_address3), 0 },
 };
